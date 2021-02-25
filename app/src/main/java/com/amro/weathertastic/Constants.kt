f6488cfc2712e12ca72e81ddb017373aedecac28
Amro <amrmu94@gmail.com>
2021-02-25 05:24:11 +0200

    finished autoComplete fragment

---
+++ app/src/main/java/com/amro/weathertastic/Constants.kt
@@ -0,0 +1,7 @@
+package com.amro.weathertastic
+
+object Constants {
+    val MAPBOX_API_KEY = "pk.eyJ1IjoiYW1ybXVoYW1tYWQiLCJhIjoiY2tsazc2dGxxMDF6YjJ3bXdpY2ZpbWhxOCJ9.-ig2mB_01Yj0WVkiUtfeMw"
+    val AUTOCOMPLETE_FRAGMENT_TAG = "autoCompleteFragment"
+    val LOG_TAG = "test"
+}
--- app/src/main/java/com/amro/weathertastic/FavouriteFragment.kt
+++ app/src/main/java/com/amro/weathertastic/FavouriteFragment.kt
@@ -1,35 +1,73 @@
 package com.amro.weathertastic
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.navArgs
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import androidx.lifecycle.ViewModelProvider
 import com.amro.weathertastic.databinding.FavouriteFragmentBinding
-import com.amro.weathertastic.databinding.HomeFragmentBinding
+import com.mapbox.api.geocoding.v5.models.CarmenFeature
+import com.mapbox.mapboxsdk.plugins.places.autocomplete.ui.PlaceAutocompleteFragment
+import com.mapbox.mapboxsdk.plugins.places.autocomplete.ui.PlaceSelectionListener
 
 class FavouriteFragment : Fragment() {
 
     private lateinit var viewModel: FavouriteViewModel
     private var _binding: FavouriteFragmentBinding? = null
     private val binding get() = _binding!!
+    private var transaction : FragmentTransaction? = null
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        _binding = FavouriteFragmentBinding.inflate(inflater,container,false)
-        return binding!!.root
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FavouriteFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         viewModel = ViewModelProvider(this).get(FavouriteViewModel::class.java)
         // TODO: Use the ViewModel
+
+        binding.favFloatingButton.setOnClickListener {
+            showAutoCompleteBar()
+        }
     }
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun showAutoCompleteBar(){
+        binding.searchFragmentContainer.visibility= View.VISIBLE
+        val autocompleteFragment = PlaceAutocompleteFragment.newInstance(Constants.MAPBOX_API_KEY)
+            transaction = activity?.supportFragmentManager?.beginTransaction()
+            transaction?.add(R.id.search_fragment_container, autocompleteFragment, Constants.AUTOCOMPLETE_FRAGMENT_TAG)
+            transaction?.commit()
+
+        autocompleteFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+            override fun onPlaceSelected(carmenFeature: CarmenFeature) {
+                // TODO: Use the longitude and latitude
+                Toast.makeText(context,"latitude ${carmenFeature.center()?.latitude()} \n longitude ${carmenFeature.center()?.longitude()}"
+                    , Toast.LENGTH_LONG).show()
+                activity?.supportFragmentManager?.beginTransaction()?.remove(autocompleteFragment)?.commit()
+                binding.searchFragmentContainer.visibility= View.GONE
+            }
+
+            override fun onCancel() {
+                Log.i(Constants.LOG_TAG,"cancel")
+                activity?.supportFragmentManager?.beginTransaction()?.remove(autocompleteFragment)?.commit()
+                binding.searchFragmentContainer.visibility= View.GONE
+            }
+        })
     }
 
 }
