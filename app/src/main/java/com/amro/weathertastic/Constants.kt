9aa38953a99bb0dce0a969a666f6253300bcb5ca
Amro <amrmu94@gmail.com>
2021-02-25 21:07:53 +0200

    finished gps and current location

--- app/src/main/java/com/amro/weathertastic/Constants.kt
+++ app/src/main/java/com/amro/weathertastic/Constants.kt
@@ -1,7 +1,11 @@
 package com.amro.weathertastic
 
 object Constants {
-    val MAPBOX_API_KEY = "pk.eyJ1IjoiYW1ybXVoYW1tYWQiLCJhIjoiY2tsazc2dGxxMDF6YjJ3bXdpY2ZpbWhxOCJ9.-ig2mB_01Yj0WVkiUtfeMw"
-    val AUTOCOMPLETE_FRAGMENT_TAG = "autoCompleteFragment"
-    val LOG_TAG = "test"
+    const val MAPBOX_API_KEY = "pk.eyJ1IjoiYW1ybXVoYW1tYWQiLCJhIjoiY2tsazc2dGxxMDF6YjJ3bXdpY2ZpbWhxOCJ9.-ig2mB_01Yj0WVkiUtfeMw"
+    const val AUTOCOMPLETE_FRAGMENT_TAG = "autoCompleteFragment"
+    const val LOG_TAG = "test"
+    const val LOCATION_PERMISSION_CODE = 202
+    const val CURRENT_LONGITUDE = "currentLongitude"
+    const val CURRENT_LATITUDE = "currentLatitude"
+
 }
--- app/src/main/java/com/amro/weathertastic/MainActivity.kt
+++ app/src/main/java/com/amro/weathertastic/MainActivity.kt
@@ -1,10 +1,19 @@
 package com.amro.weathertastic
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
+import android.os.Looper
+import android.provider.Settings
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
@@ -13,6 +22,10 @@ import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.amro.weathertastic.databinding.ActivityMainBinding
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -43,6 +56,12 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    override fun onStart() {
+        super.onStart()
+        //latest location and permission
+        getLatestLocation()
+    }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.options_menu, menu)
         return true
@@ -61,5 +80,71 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    //request Location Permission
+    @SuppressLint("MissingPermission")
+    fun getLatestLocation() {
+        if (isPermissionGranted()) {
+            if (checkLocation()) {
+                val locationRequest = LocationRequest()
+                with(locationRequest) {
+                    priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+                    interval = 1000
+                    setNumUpdates(2)
+                }
+                val fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+                fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper())
+            } else {
+                enableLocation()
+            }
+        } else {
+            requestPermission()
+        }
+    }
+
+    private fun isPermissionGranted(): Boolean {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun checkLocation(): Boolean {
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+    }
+
+    private val locationCallback = object : LocationCallback(){
+        override fun onLocationResult(locationResult: LocationResult) {
+            val location = locationResult.lastLocation
+            // TODO use current location long and lat
+            Toast.makeText(this@MainActivity, "Latitude:${location.latitude}\nlongitude:${location.longitude}", Toast.LENGTH_LONG).show()
+            saveCurrentLocationToSharedPref(location.latitude.toString(),location.longitude.toString())
+        }
+    }
+
+    private fun saveCurrentLocationToSharedPref(latitude: String,longitude: String){
+        val sharedPref = getSharedPreferences("currentLocation", MODE_PRIVATE)
+        val editor = sharedPref.edit()
+        editor.putString(Constants.CURRENT_LATITUDE,latitude).apply()
+        editor.putString(Constants.CURRENT_LONGITUDE,longitude).apply()
+    }
+
+    private fun enableLocation() {
+        val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+        startActivity(intent)
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION), Constants.LOCATION_PERMISSION_CODE)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == Constants.LOCATION_PERMISSION_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                getLatestLocation()
+        }
     }
 }
